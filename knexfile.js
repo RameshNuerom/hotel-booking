a89ee630846823901cfb6e7547e82cade7d6b2f4
Sumedhsonkamble <32011242+Sumedhsonkamble@users.noreply.github.com>
2025-07-22 18:03:38 +0530

    updated knex file

--- knexfile.js
+++ knexfile.js
@@ -5,21 +5,18 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.DB_HOST || '127.0.0.1',
-      port: process.env.DB_PORT || 5432,
-      database: process.env.DB_NAME || 'hotel_booking_db',
-      user: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASSWORD || '',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
     migrations: {
-      directory: './migrations',
+      tableName: 'knex_migrations',
+      directory: './migrations'
     },
     seeds: {
-      directory: './seeds',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
+      directory: './seeds'
+    }
   },
 };
